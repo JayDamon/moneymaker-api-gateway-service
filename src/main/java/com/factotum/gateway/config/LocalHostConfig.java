81eb7b41bf8fe25bc0f52655a9bfe6a657f8b514
= <Jay.n.damon@gmail.com>
2021-07-26 20:44:04 -0400

    feat: add local docker and reinstate LocalHostConfig

--- src/main/java/com/factotum/gateway/config/LocalHostConfig.java
+++ src/main/java/com/factotum/gateway/config/LocalHostConfig.java
@@ -1,23 +1,23 @@
-//package com.factotum.gateway.config;
-//
-//import org.springframework.cloud.gateway.route.RouteLocator;
-//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class LocalHostConfig {
-//
-//    @Bean
-//    public RouteLocator localHostRoutes(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route(r -> r.path("/v1/accounts/**")
-//                        .uri("lb://moneymaker-account-service/"))
-//                .route(r -> r.path("/v1/transactions/**")
-//                        .uri("lb://moneymaker-transaction-service/"))
-//                .route(r -> r.path("/v1/budgets/**")
-//                        .uri("lb://moneymaker-budget-service/"))
-//                .build();
-//    }
-//
-//}
+package com.factotum.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LocalHostConfig {
+
+    @Bean
+    public RouteLocator localHostRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/v1/accounts/**")
+                        .uri("lb://moneymaker-account-service/"))
+                .route(r -> r.path("/v1/transactions/**")
+                        .uri("lb://moneymaker-transaction-service/"))
+                .route(r -> r.path("/v1/budgets/**")
+                        .uri("lb://moneymaker-budget-service/"))
+                .build();
+    }
+
+}
