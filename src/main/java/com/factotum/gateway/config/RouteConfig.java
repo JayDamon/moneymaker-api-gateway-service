8464330edeb8623a7a76d2d4cabd1f94b91779a7
= <Jay.n.damon@gmail.com>
2021-08-05 08:40:19 -0400

    feat: Add key passthrough and prepare for endpoint lockdown

--- src/main/java/com/factotum/gateway/config/RouteConfig.java
+++ src/main/java/com/factotum/gateway/config/RouteConfig.java
@@ -1,21 +1,27 @@
 package com.factotum.gateway.config;
 
+import org.springframework.cloud.gateway.filter.factory.TokenRelayGatewayFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class LocalHostConfig {
+public class RouteConfig {
 
     @Bean
-    public RouteLocator localHostRoutes(RouteLocatorBuilder builder) {
+    public RouteLocator localHostRoutes(RouteLocatorBuilder builder, TokenRelayGatewayFilterFactory filterFactory
+    ) {
         return builder.routes()
                 .route(r -> r.path("/v1/accounts/**")
-                        .uri("lb://moneymaker-account-service/"))
+                        .filters(f -> f.filter(filterFactory.apply()))
+                        .uri("lb://moneymaker-account-service/")
+                )
                 .route(r -> r.path("/v1/transactions/**")
+                        .filters(f -> f.filter(filterFactory.apply()))
                         .uri("lb://moneymaker-transaction-service/"))
                 .route(r -> r.path("/v1/budgets/**")
+                        .filters(f -> f.filter(filterFactory.apply()))
                         .uri("lb://moneymaker-budget-service/"))
                 .build();
     }
