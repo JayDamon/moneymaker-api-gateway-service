1f8c2eb18c9f4cd6ce4f35298a4df50b7e75c6a5
= <Jay.n.damon@gmail.com>
2021-08-05 21:24:24 -0400

    refactor(config): change route config method name

--- src/main/java/com/factotum/gateway/config/RouteConfig.java
+++ src/main/java/com/factotum/gateway/config/RouteConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 public class RouteConfig {
 
     @Bean
-    public RouteLocator localHostRoutes(RouteLocatorBuilder builder, TokenRelayGatewayFilterFactory filterFactory
+    public RouteLocator routes(RouteLocatorBuilder builder, TokenRelayGatewayFilterFactory filterFactory
     ) {
         return builder.routes()
                 .route(r -> r.path("/v1/accounts/**")
