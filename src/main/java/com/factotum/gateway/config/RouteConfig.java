d6bc796dcf37cd852e400e0cdc34b971c40984ad
Jay Damon <jay.n.damon@gmail.com>
2023-02-25 07:21:28 -0500

    BREAKING CHANGE: Upgrade to spring boot 3.0 and spring cloud 2022

--- src/main/java/com/factotum/gateway/config/RouteConfig.java
+++ src/main/java/com/factotum/gateway/config/RouteConfig.java
@@ -1,5 +1,6 @@
 package com.factotum.gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.factory.TokenRelayGatewayFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
@@ -9,20 +10,37 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RouteConfig {
 
+    private final String userServiceUri;
+    private final String plaidServiceUri;
+
+    public RouteConfig(
+            @Value("${user-service.url:lb://moneymaker-user-service/}") String userServiceUri,
+            @Value("${account-link-service.url:lb://moneymaker-account-link-service/}") String plaidServiceUri) {
+        this.userServiceUri = userServiceUri;
+        this.plaidServiceUri = plaidServiceUri;
+    }
+
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder, TokenRelayGatewayFilterFactory filterFactory
     ) {
+        System.out.println(this.userServiceUri);
+        System.out.println(this.plaidServiceUri);
         return builder.routes()
                 .route(r -> r.path("/v1/accounts/**")
                         .filters(f -> f.filter(filterFactory.apply()))
-                        .uri("lb://moneymaker-account-service/")
-                )
+                        .uri("lb://moneymaker-account-service/"))
                 .route(r -> r.path("/v1/transactions/**")
                         .filters(f -> f.filter(filterFactory.apply()))
                         .uri("lb://moneymaker-transaction-service/"))
                 .route(r -> r.path("/v1/budgets/**")
                         .filters(f -> f.filter(filterFactory.apply()))
                         .uri("lb://moneymaker-budget-service/"))
+                .route(r -> r.path("/v1/users/**")
+                        .filters(f -> f.filter(filterFactory.apply()))
+                        .uri(this.userServiceUri))
+                .route(r -> r.path("/v1/link/**", "/v1/item/**")
+                        .filters(f -> f.filter(filterFactory.apply()))
+                        .uri(this.plaidServiceUri))
                 .build();
     }
 
